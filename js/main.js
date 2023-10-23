a56150955e830b01e013f3e2b10a02561a3954ff
ezedesa <ezequiel.194300@gmail.com>
2023-10-23 02:52:40 -0300

    fix fonts

--- js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const checkbox = document.getElementById('checkbox');
-    const icono = document.getElementById('barra');
-  
-    icono.addEventListener('click', function () {
-      checkbox.checked = !checkbox.checked;
-    });
-  });
-  
